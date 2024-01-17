7dbda12dedd15333bdbfe8c5cf2ef5b9c3674f8d
Marcus Reinhardt <webstone@gmail.com>
2024-01-18 00:38:41 +0100

    fix linting issue

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,25 +1,15 @@
-import type { Session, User } from "lucia"
-import { cache } from "react"
-import { cookies } from "next/headers"
-import { DrizzleMySQLAdapter } from "@lucia-auth/adapter-drizzle"
-import { Lucia } from "lucia"
+import type { Session, User } from "lucia";
+import { cache } from "react";
+import { cookies } from "next/headers";
+import { DrizzleMySQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { Lucia } from "lucia";
 
-import { db, schema } from "@acme/db"
+import { db, schema } from "@acme/db";
 
-import * as discordProvider from "./providers/discord"
-import * as githubProvider from "./providers/github"
+import * as discordProvider from "./providers/discord";
+import * as githubProvider from "./providers/github";
 
-type AuthResponse =
-  | { user: User; session: Session }
-  | { user: null; session: null }
-declare module "lucia" {
-  interface Register {
-    Lucia: typeof lucia
-    DatabaseUserAttributes: Omit<typeof schema.users.$inferSelect, "id">
-  }
-}
-
-const adapter = new DrizzleMySQLAdapter(db, schema.sessions, schema.users)
+const adapter = new DrizzleMySQLAdapter(db, schema.sessions, schema.users);
 
 export const lucia = new Lucia(adapter, {
   sessionCookie: {
@@ -30,47 +20,57 @@ export const lucia = new Lucia(adapter, {
   getUserAttributes: (attributes) => {
     return {
       name: attributes.name,
-    }
+    };
   },
-})
+});
 
 export const auth = cache(async (): Promise<AuthResponse> => {
-  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
   if (!sessionId) {
     return {
       user: null,
       session: null,
-    }
+    };
   }
 
-  const result = await lucia.validateSession(sessionId)
+  const result = await lucia.validateSession(sessionId);
   // next.js throws when you attempt to set cookie when rendering page
   try {
     if (result.session && result.session.fresh) {
-      const sessionCookie = lucia.createSessionCookie(result.session.id)
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
       cookies().set(
         sessionCookie.name,
         sessionCookie.value,
         sessionCookie.attributes,
-      )
+      );
     }
     if (!result.session) {
-      const sessionCookie = lucia.createBlankSessionCookie()
+      const sessionCookie = lucia.createBlankSessionCookie();
       cookies().set(
         sessionCookie.name,
         sessionCookie.value,
         sessionCookie.attributes,
-      )
+      );
     }
   } catch {
     /* empty */
   }
-  return result
-})
+  return result;
+});
 
 export const providers = {
   github: githubProvider,
   discord: discordProvider,
-} as const
+} as const;
 
-export type Providers = keyof typeof providers
+export type Providers = keyof typeof providers;
+
+export type AuthResponse =
+  | { user: User; session: Session }
+  | { user: null; session: null };
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: Omit<typeof schema.users.$inferSelect, "id">;
+  }
+}
